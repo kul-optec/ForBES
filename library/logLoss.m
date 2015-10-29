922bb06b618b742b8974bacddcbd3962ec73950e
Lorenzo Stella <lorenzostella@gmail.com>
2015-10-29 22:12:19 +0100

    fixed logistic function

--- library/logLoss.m
+++ library/logLoss.m
@@ -1,24 +1,24 @@
 %LOGLOSS Allocates the log-logistic loss function.
 %
 %   LOGLOSS(mu) builds the log-logistic loss function
-%       
+%
 %       f(x) = mu*(sum_i log(1+exp(-x_i)))
 %
 
 % Copyright (C) 2015, Lorenzo Stella and Panagiotis Patrinos
 %
 % This file is part of ForBES.
-% 
+%
 % ForBES is free software: you can redistribute it and/or modify
 % it under the terms of the GNU Lesser General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
-% 
+%
 % ForBES is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 % GNU Lesser General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU Lesser General Public License
 % along with ForBES. If not, see <http://www.gnu.org/licenses/>.
 
@@ -32,16 +32,16 @@ function obj = logLoss(mu)
 end
 
 function [val, grad, hess] = call_logLoss_f(x, mu)
-% value and gradient of f(x) = mu*sum(log(1+exp(-x)))
-    ex = exp(x);
-    px = ex./(1+ex);
-    val = -sum(log(px))*mu;
-    if nargout >= 2
-        grad = (px-1)*mu;
-    end
+  % value and gradient of f(x) = mu*sum(log(1+exp(-x)))
+  emx = exp(-x);
+  invpx = (1+emx);
+  val = sum(log(invpx))*mu;
+  if nargout >= 2
+    px = 1./invpx;
+    grad = (px-1)*mu;
     if nargout >= 3
-        h = (mu*ex)./((ex+1).^2);
-%         hess = @(y) h.*y;
-        hess = diag(sparse(h));
+      h = px.*(1-px);
+      hess = diag(sparse(h));
     end
+  end
 end
