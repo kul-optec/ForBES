4e5c03a727dd751e39dad3c33087188e96a2b960
Lorenzo Stella <lorenzostella@gmail.com>
2017-03-07 16:41:41 +0100

    fixed solver

--- private/classical.m
+++ private/classical.m
@@ -1,8 +1,16 @@
 function out = classical(prob, opt, lsopt)
 
-% initialize operations counter
+% initialize output stuff
 
-ops = FBOperations();
+if opt.report
+    residual = zeros(1, opt.maxit);
+    objective = zeros(1, opt.maxit);
+    ts = zeros(1, opt.maxit);
+    % initialize operations counter
+    ops = FBOperations();
+else
+    ops = [];
+end
 
 % get Lipschitz constant & adaptiveness
 
@@ -25,6 +33,8 @@ t0 = tic();
 cache_current = FBCache(prob, prob.x0, gam, ops);
 restart = 0;
 
+t0 = tic();
+
 for it = 1:opt.maxit
 
     % trace stuff
@@ -33,13 +43,14 @@ for it = 1:opt.maxit
         cache_0 = cache_current;
     end
 
-    residual(1, it) = norm(cache_current.Get_FPR(), 'inf')/cache_current.Get_Gamma();
-    objective(1, it) = cache_current.Get_FBE();
-
-    ts(1, it) = toc(t0);
+    if opt.report
+        residual(1, it) = norm(cache_current.Get_FPR(), 'inf')/cache_current.Get_Gamma();
+        objective(1, it) = cache_current.Get_FBE();
+        ts(1, it) = toc(t0);
+    end
 
     if opt.toRecord
-        record(:, it) = opt.record(prob, it, gam, cache_0, cache_current, ops);
+        record(:, it) = opt.record(prob, it, cache_0, cache_current);
     end
 
     solution = cache_current.z;
@@ -59,8 +70,8 @@ for it = 1:opt.maxit
                 break;
             end
         else
-            flagStop = opt.term(prob, it, gam, cache_0, cache_current, ops);
-            if (opt.adaptive == 0 || it > 1) && flagStop
+            flagStop = opt.term(prob, it, cache_0, cache_current);
+            if (adaptive == 0 || it > 1) && flagStop
                 msgTerm = 'reached optimum (custom criterion)';
                 flagTerm = 0;
                 break;
@@ -117,6 +128,8 @@ for it = 1:opt.maxit
 
 end
 
+time = toc(t0);
+
 if it == opt.maxit
     flagTerm = 1;
     msgTerm = 'exceeded maximum iterations';
@@ -134,9 +147,12 @@ out.flag = flagTerm;
 out.x = solution;
 out.iterations = it;
 out.operations = ops;
-out.residual = residual(1, 1:it);
-out.objective = objective(1, 1:it);
-out.ts = ts(1, 1:it);
-if opt.toRecord, out.record = record; end
+if opt.report
+    out.residual = residual(1, 1:it);
+    out.objective = objective(1, 1:it);
+    out.ts = ts(1, 1:it);
+end
+out.record = record;
 out.gam = gam;
-out.skip = cacheDir.cntSkip;
+out.adaptive = adaptive;
+out.time = time;
