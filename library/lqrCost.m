fc7bdfd699361a9c18f5cd59400030d7b13f2a11
Lorenzo Stella <lorenzostella@gmail.com>
2017-08-20 11:50:50 +0200

    more efficient conjugate

--- library/lqrCost.m
+++ library/lqrCost.m
@@ -61,6 +61,7 @@ function obj = lqrCost(x0, varargin)
         obj.A = varargin{4};
         obj.B = varargin{5};
         obj.N = varargin{6};
+        obj.QR = blkdiag(obj.Q, obj.R);
         obj = RiccatiFactor(obj);
         if length(varargin) >= 7
             xref = varargin{7};
@@ -95,12 +96,18 @@ end
 function [fcw, xu] = call_lqrCost_fconj(w, x0, obj)
     [n_x, n_u] = size(obj.B);
     [~, xu] = RiccatiSolve(w+obj.tilt, x0, obj.A, obj.B, obj.LRs, obj.Ks, obj.Ms, obj.Ls, int32(n_x), int32(n_u), int32(obj.N));
-    fxu = 0;
-    for i=0:obj.N-1
-        x_i = xu(i*(n_x+n_u)+1:i*(n_x+n_u)+n_x);
-        u_i = xu(i*(n_x+n_u)+n_x+1:(i+1)*(n_x+n_u));
-        fxu = fxu + 0.5*(x_i'*(obj.Q*x_i) + u_i'*(obj.R*u_i));
-    end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Less efficient
+%     fxu = 0;
+%     for i=0:obj.N-1
+%         x_i = xu(i*n_xu+1:i*n_xu+n_x);
+%         u_i = xu(i*n_xu+n_x+1:(i+1)*n_xu);
+%         fxu = fxu + 0.5*(x_i'*(obj.Q*x_i) + u_i'*(obj.R*u_i));
+%     end
+    % More efficient
+    XU_stage = reshape(xu(1:end-n_x), n_x + n_u, obj.N);
+    fxu = 0.5*sum(sum(XU_stage.*(obj.QR*XU_stage)));
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     x_N = xu(obj.N*(n_x+n_u)+1:end);
     fxu = fxu + 0.5*(x_N'*(obj.Q_f*x_N));
     fcw = (w+obj.tilt)'*xu - fxu - obj.diff;
